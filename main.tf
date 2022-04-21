ba2608ec75c03054a5b35020752f59c2b0e4cea1
Terence Luk <terence.luk@gmail.com>
2022-04-20 20:20:18 -0400

    Updated
    Updaing files

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ The source argument in a module block tells Terraform where to find the source c
 Terraform uses this during the module installation step of terraform init to download the source code to a directory on local disk so that it can be used by other Terraform commands.
 */
 
-# Module for deploying Azure Kubernetes
+## Module for deploying Azure Kubernetes
 module "setup_azure_kubernetes" {
   source                  = "github.com/terenceluk/terraform-test//Modules/AKS"
   cluster_name            = var.cluster_name
